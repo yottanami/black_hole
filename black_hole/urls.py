d0776bc1cddc6a4bcb7f823269da932240fc83ce
yottanami <yottanami@gmail.com>
2019-03-27 01:50:23 +0430

    Create creadit app

--- black_hole/urls.py
+++ black_hole/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView # new
+from credit import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('creadit/', views.index),
     path('accounts/', include('allauth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 
