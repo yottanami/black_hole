b20a29ec2c4e7c3cb13b771442585af1c7d23358
yottanami <yottanami@gmail.com>
2019-03-27 03:17:06 +0430

    Fix credit urls

--- black_hole/urls.py
+++ black_hole/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView # new
-from credit import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('creadit/', views.index),
+    path('creadit/', include('credit.urls')),
     path('accounts/', include('allauth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-
 ]
