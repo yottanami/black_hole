4bb5fa599f0eff961c5dbc4be6520b61d9f47343
yottanami <yottanami@gmail.com>
2019-04-03 20:40:46 +0430

    Add payment requirements

--- black_hole/urls.py
+++ black_hole/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView # new
+from django.views.generic.base import TemplateView
+from pardakht import urls as pardakht_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('payment/', include('pardakht.urls')),
     path('creadit/', include('credit.urls')),
     path('accounts/', include('allauth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
--- credit/views.py
+++ credit/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pardakht import handler
 
 
 def index(request):
-    return HttpResponse('Creadit')
+    result = handler.create_payment(
+        20000,
+        'description',
+        ret,
+        'return_url',
+        False
+    )
+    return HttpResponse(result['link'])
+
+def ret():
+    return HttpResponse('Res')
