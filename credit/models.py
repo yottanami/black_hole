f58a83cef4616c33616df81b2c4c0a2323e2d690
yottanami <yottanami@gmail.com>
2019-04-09 17:41:46 +0430

    Create invoice model

--- credit/models.py
+++ credit/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Invoice(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    deposit = models.IntegerField(min_digits=4)
+    withdraw = models.IntegerField(min_digits=4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
